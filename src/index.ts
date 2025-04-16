4b14f0ca365001a2841ea49fa8e317140334e3d9
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-04-16 16:11:45 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import { logger } from 'hono/logger'
 
 import { cors } from 'hono/cors'
 import { secureHeaders } from 'hono/secure-headers'
-
-const app = new Hono().basePath('/api')
+import { authMiddleware, TAuthVariables } from './middlewares/auth.middleware'
+const app = new Hono().basePath('/api/subscription')
 .use('/api/*', cors())
 .use(secureHeaders())
 .use(logger())
+.use('*', authMiddleware)
 .get('/', async (c) => {
     return c.text("hello world from subscription", 200)
 })
